e4c330c92916d332b980b9c4d3579ace60849fd4
Ashish <ashishredhu5810@gmail.com>
2024-09-14 21:47:24 +0530

    Feature Added: Navbar background on scrollY  and Testimonials SliderX

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Navbar from './Components/Navbar/Navbar.jsx'
 import Hero from './Components/Hero/Hero.jsx'
 import Programs from './Components/Programs/Programs.jsx'
 import Title from './Components/Title/Title.jsx'
-
+import About from './Components/About/About.jsx'
+import Campus from './Components/Campus/Campus.jsx'
+import Testimonials from './Components/Testimonials/Testimonials.jsx'
 
 export default function App() {
   return (
@@ -13,6 +15,11 @@ export default function App() {
       <div className="container">
         <Title subtitle="OUR PROGRAM" title="What We Offer"/>
         <Programs/>
+        <About/>
+        <Title subtitle="Gallery " title="Campus Photos"/>
+        <Campus/>
+        <Title subtitle="TESTIMONIALS" title="What Student Says"/>
+        <Testimonials/>
       </div>
       
     </div>

cbabfa0a86aad380b6b3960124f96457b57e6942
Ashish <ashishredhu5810@gmail.com>
2024-09-15 00:30:01 +0530

    Responsiveness added

---
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,18 @@
+import React, {useRef} from 'react'
+import './VideoPlayer.css'
+import video from '../../assets/univideo.mp4'
+
+export default function VideoPlayer({playState, setPlayState}) {
+    const player = useRef(null);
+    const closePlayer = (e)=>{
+        if(e.target===player.current){
+            setPlayState(false);
+        }
+    }
+
+  return (
+    <div className={`video-player ${playState?'':'hide'}`} ref={player} onClick={closePlayer}>
+        <video src={video} autoPlay muted controls loop></video>
+    </div>
+  )
+}
